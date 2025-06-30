251b1c1df2c798d0d3dd6f4ec36b3275c3af62de
Tommy Costa <tommyscosta@gmail.com>
2025-06-30 13:02:03 -0700

    Adjusted retailer_db_generator.py logic to more reflect item and order state interactions more realistically. Added screenshots of example outputs.

--- retailer_db_generator.py
+++ retailer_db_generator.py
@@ -33,8 +33,14 @@ for i in range(5000):
     cursor.execute("INSERT INTO items (itemState, stockCount) VALUES (?, ?)", (itemState, stockCount))
     itemId = cursor.lastrowid
 
+    if itemState == "Ordered":
+        orderState = "Paid"
+    elif itemState == "Fulfilled":
+        orderState = random.choice(["Processed", "InTransit"])
+    elif itemState == "Delivered":
+        orderState = "Delivered"
+
     if itemState in ("Ordered", "Fulfilled", "Delivered"):
-        orderState = random.choice(orderStates)
         orderDatetime = datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0, 60*60*24*90))
         orderTime = orderDatetime.isoformat(" ")
         cursor.execute("INSERT INTO orders (itemId, orderState, orderTime) VALUES (?, ?, ?)", (itemId, orderState, orderTime))
